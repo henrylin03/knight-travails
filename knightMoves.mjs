86874e48c7a61a7fc0a8091a062b6749468c04cf
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-30 20:11:07 +1000

    correctly identify the shortest amount of moves between two coordinates, by tracking the distance as a 'weight' as the last element of the coordinates array

--- knightMoves.mjs
+++ knightMoves.mjs
@@ -5,17 +5,17 @@ const isSameArray = (arr1, arr2) =>
   JSON.stringify(arr1) === JSON.stringify(arr2);
 
 export default function knightMoves(startPosition, endPosition) {
-  const coordinatesQueue = [startPosition];
-  const visited = new Set();
   let movesCount = 0;
+  const coordinatesQueue = [[...startPosition, movesCount]]; // weighted with depth
+  const visited = new Set();
 
   while (coordinatesQueue.length) {
-    console.log("---");
-    console.log("movesCount:", movesCount);
+    console.log("\n--start--");
     console.log("coordinatesQueue:", coordinatesQueue);
-    // console.log("visited positions:", visited);
-    const position = coordinatesQueue.shift();
 
+    const node = coordinatesQueue.shift();
+    const position = node.slice(0, 2);
+    movesCount = node[2];
     console.log("current position:", position);
 
     if (isSameArray(position, endPosition)) break;
@@ -26,17 +26,11 @@ export default function knightMoves(startPosition, endPosition) {
     const validNextMoves = knight.getValidNextMoves();
     if (validNextMoves.has(endPosition)) break;
     validNextMoves.forEach((position) => {
-      if (!visited.has(position)) coordinatesQueue.push(position);
+      if (!visited.has(position))
+        coordinatesQueue.push([...position, movesCount]);
     });
+    console.log("--end--\n");
   }
-
-  // at every position, starting from start position, generate set of valid next moves
-  // check if the end position is within those moves - if yes, end and print those statements required (see index.mjs)
-  // if not, then find the next layer down of valid moves, check again
-  //then trace?? the shortest path once found?? NEEDS RECURSION PROBABLY
-  //implement BFS because it's a tree of potential moves - same height - can't really go back up the tree
-
-  //track visited squares too so you never go back to it
   console.log("found!!", movesCount, "moves");
 
   return;
