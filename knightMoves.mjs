047f60e6b7f055b3d7c4f3be96b42f98342a4ad9
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-31 10:56:00 +1000

    return a node from a helper function within knightmoves because node has all the info we need so far. fix: ensure visitedcoordinates are updated with every iteration of while loop. fix: stringify coordinates first into the set to actually check if the array is in the set

--- knightMoves.mjs
+++ knightMoves.mjs
@@ -3,24 +3,32 @@ import createNode from "./createNode.mjs";
 const isSameArr = (arr1, arr2) => JSON.stringify(arr1) === JSON.stringify(arr2);
 
 export default function knightMoves(startCoordinates, endCoordinates) {
-  const startNode = createNode(startCoordinates);
-  const nodesQueue = [startNode];
-  const visitedCoordinates = new Set();
+  const findNodeAtEndCoordinates = () => {
+    const startNode = createNode(startCoordinates);
+    const nodesQueue = [startNode];
+    const visitedCoordinates = new Set();
 
-  while (nodesQueue.length > 0) {
-    const currentNode = nodesQueue.shift();
-    console.log("currentNode:", currentNode); //TODO: delete - this is for debugging
-    if (isSameArr(currentNode.coordinates, endCoordinates)) break; // may need to wrap this whole part into another method/function so that it returns the node, and then we read the node's details
+    while (nodesQueue.length > 0) {
+      const currentNode = nodesQueue.shift();
+      console.log("currentNode:", currentNode);
+      if (isSameArr(currentNode.coordinates, endCoordinates))
+        return currentNode;
 
-    let currentDepth = currentNode.depth;
-    const nextCoordinates = currentNode.getNextCoordinates();
-    nextCoordinates.forEach((coordinates) => {
-      if (visitedCoordinates.has(coordinates)) return; // skip visited coordinates
-      const newNode = createNode(coordinates, currentDepth + 1);
-      newNode.previousNode = currentNode;
-      nodesQueue.push(newNode);
-    });
-  }
+      let currentDepth = currentNode.depth;
+      const nextCoordinates = currentNode.getNextCoordinates();
+      nextCoordinates.forEach((coordinates) => {
+        if (visitedCoordinates.has(JSON.stringify(coordinates))) return; // skip visited coordinates
+        visitedCoordinates.add(JSON.stringify(coordinates)); // ensure matches by turning into string
 
-  console.log("FOUND!");
+        const newNode = createNode(coordinates, currentDepth + 1);
+        newNode.previousNode = currentNode;
+        nodesQueue.push(newNode);
+      });
+    }
+  };
+
+  const matchingNode = findNodeAtEndCoordinates();
+  console.log(`You made it in ${matchingNode.depth} moves! Here's your path:
+${matchingNode.previousNode.coordinates}
+`);
 }
