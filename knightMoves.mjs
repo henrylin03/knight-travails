2f7add23bfceaf25768c93957944c549a299e722
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-31 10:17:43 +1000

    initialise a start node, add the start node to the nodesQueue initially, and initialise a set to check visited nodes
    it would be good to have access to the visited nodes in the createNodes' .getNextMoves method so that we only return those that haven't been visited. it would be unclean to pass the visitedNodes into every call of the node factory. let me think about this some more

--- knightMoves.mjs
+++ knightMoves.mjs
@@ -1,36 +1,42 @@
 // import createKnight from "./createKnight.mjs";
+import createNode from "./createNode.mjs";
 
-// helper to compare arrays
+export default function knightMoves(startCoordinates, endCoordinates) {
+  const startNode = createNode(startCoordinates);
+  const nodesQueue = [startNode];
+  const visitedNodes = new Set();
 
 
-export default function knightMoves(startPosition, endPosition) {
-  let movesCount = 0;
-  const coordinatesQueue = [[...startPosition, movesCount]]; // weighted with depth
-  const visited = new Set();
-
-  while (coordinatesQueue.length) {
-    console.log("\n--start--");
-    console.log("coordinatesQueue:", coordinatesQueue);
-
-    const node = coordinatesQueue.shift();
-    const position = node.slice(0, 2);
-    movesCount = node[2];
-    console.log("current position:", position);
-
-    if (isSameArray(position, endPosition)) break;
-    visited.add(position);
-    movesCount++;
-
-    const knight = createKnight(position);
-    const validNextMoves = knight.getValidNextMoves();
-    if (validNextMoves.has(endPosition)) break;
-    validNextMoves.forEach((position) => {
-      if (!visited.has(position))
-        coordinatesQueue.push([...position, movesCount]);
-    });
-    console.log("--end--\n");
-  }
-  console.log("found!!", movesCount, "moves");
-
-  return;
 }
+
+// export default function knightMoves(startPosition, endPosition) {
+//   let movesCount = 0;
+//   const coordinatesQueue = [[...startPosition, movesCount]]; // weighted with depth
+//   const visited = new Set();
+
+//   while (coordinatesQueue.length) {
+//     console.log("\n--start--");
+//     console.log("coordinatesQueue:", coordinatesQueue);
+
+//     const node = coordinatesQueue.shift();
+//     const position = node.slice(0, 2);
+//     movesCount = node[2];
+//     console.log("current position:", position);
+
+//     if (isSameArray(position, endPosition)) break;
+//     visited.add(position);
+//     movesCount++;
+
+//     const knight = createKnight(position);
+//     const validNextMoves = knight.getValidNextMoves();
+//     if (validNextMoves.has(endPosition)) break;
+//     validNextMoves.forEach((position) => {
+//       if (!visited.has(position))
+//         coordinatesQueue.push([...position, movesCount]);
+//     });
+//     console.log("--end--\n");
+//   }
+//   console.log("found!!", movesCount, "moves");
+
+//   return;
+// }
